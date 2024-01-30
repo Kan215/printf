ff04b9b04d393a8d870185baf020e21f20023c01
PAULNDEDE <paulodhiambo193@gmail.com>
2024-01-30 11:21:07 -0800

    printf

--- _printf.c
+++ _printf.c
@@ -32,12 +32,12 @@ int _printf(const char *format, ...)
 		else
 		{
 			print_buffer(buffer, &buff_ind);
-			flags = get_flags(format, &i);
+			flags = extract_flags(format, &i);
 			width = get_width(format, &i, list);
-			precision = get_precision(format, &i, list);
+			precision = get_f(format, &i, list);
 			size = get_size(format, &i);
 			++i;
-			printed = handle_print(format, &i, list, buffer,
+			printed = h_print(format, &i, list, buffer,
 				flags, width, precision, size);
 			if (printed == -1)
 				return (-1);
